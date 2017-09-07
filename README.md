dbeaab7085d48d24155875a43243b3b8cf4cee7d
wolfbitez <wolfbitez23@gmail.com>
2017-09-07 22:10:04 +0100

    Updated bot.py & README.md
    Added comments in bot.py for improved readability, fixed README.md to use valid syntax.

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-#discordbot
+discordbot
+=============
 
 A fairly simple discord bot written in python.
 
-I'm not a person that's neat at writing code so I'll add comments later on.
-
-##Requirements:
-*discord.py
+* Requirements:
+	* discord.py
--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
 import discord
 from configparser import ConfigParser
 
+# Reads the config file..
 config = ConfigParser()
 config.read('config.ini')
+
+# Makes it easier to reference stuff from the config..
 Channel_ID = config.get('wolfbitez', 'Channel')
 Token_ID = config.get('wolfbitez', 'Token')
+
+# Initialises the bot..
 dclient = discord.Client()
+
+# "Playing" function and startup message..
 @dclient.event
 async def on_ready():
     await dclient.change_presence(game=discord.Game(name=config.get('wolfbitez', 'Playing')))
     if Channel_ID != 0:
-        await dclient.send_message(discord.Object(id=Channel_ID), "no")
+        await dclient.send_message(discord.Object(id=Channel_ID), "Successfully booted.")
+
+# Runs the bot..
 dclient.run(Token_ID)
